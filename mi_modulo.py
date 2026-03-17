19aee9180182cc605df8e533383322f1ac184ad7
facuolgg <facufdolguin@gmail.com>
2026-03-17 18:46:21 -0300

    se agregan los archivos

---
+++ mi_modulo.py
@@ -0,0 +1,7 @@
+def suma(n1: int, n2: int) -> int:
+    """Suma dos numeros enteros y devuelve el resultado"""
+    return n1 + n2
+
+def diferencia(n1: int, n2: int) -> int:
+    """Calcula la diferencia entre dos numeros enteros y devuelve el resultado"""
+    return n1 - n2
---
+++ recursividad.py
@@ -0,0 +1,53 @@
+
+# FACTORIAL !
+#factorial(5) = 5 * 4 * 3 * 2 * 1
+# 5 !
+
+#fac(5) = 5 * fac(4)
+#fac(4) = 4 * fac(3)
+#fac(3) = 3 * fac(2)
+#fac(2) = 2 * fac(1)
+#fac(1) = 1 * fac(0)
+#fac(0) = 1
+#fac(N) = N * fac(N-1) -> fac(0) = 1
+
+def factorial_r (num: int)-> int:
+    if num == 0:
+        return 1
+    else:
+        return num * factorial_r(num - 1)
+        
+
+def factorial(num: int) -> int:
+    result = 1
+    for i in range(1, num + 1):
+        result = result * i
+
+    return result
+
+print(factorial(5))
+print(factorial_r(5))
+
+
+# Fibonacci
+
+
+def fibonacci(num):
+    fib_1 = 0
+    fib_2 = 1
+    for i in range(2, num + 1):
+        fib_aux = fib_1 + fib_2
+        fib_1 = fib_2
+        fib_2 = fib_aux
+    return fib_aux
+
+print(fibonacci(50))
+
+def fibonacci_r(num):
+    if num == 0 or num == 1:
+        return num
+    else:
+        return fibonacci_r(num-1) + fibonacci_r(num-2)
+
+print(fibonacci_r(50))
+    
