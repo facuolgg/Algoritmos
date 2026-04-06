df9509f80cdcc0b1fe0df73b6ec7be45488ecb0e
facuolgg <facufdolguin@gmail.com>
2026-04-06 18:22:22 -0300

    cambios en el archivo

--- recursividad.py
+++ recursividad.py
@@ -25,8 +25,8 @@ def factorial(num: int) -> int:
 
     return result
 
-print(factorial(5))
-print(factorial_r(5))
+#print(factorial(5))
+#rint(factorial_r(5))
 
 
 # Fibonacci
@@ -41,7 +41,7 @@ def fibonacci(num):
         fib_2 = fib_aux
     return fib_aux
 
-print(fibonacci(50))
+#print(fibonacci(50))
 
 def fibonacci_r(num):
     if num == 0 or num == 1:
@@ -49,4 +49,85 @@ def fibonacci_r(num):
     else:
         return fibonacci_r(num-1) + fibonacci_r(num-2)
 
-print(fibonacci_r(50))
+#print(fibonacci_r(50))
+# print(fibonacci_r(50)) # Esto tarda mucho tiempo, no es eficiente.
+
+#Ejercicios:
+#2) suma de todos los numeros enteros comprendidos entre cero y un numero entero positivo dado
+#formula: n + (n-1) + (n-2) + ...
+# sum(n)= n + sum(n-1)                         -->  sum(0) = 0 o sum(1) = 1
+
+def sum_r(num: int) -> int:
+    if num == 0:
+        return 0
+    else:
+        return num + sum_r(num-1)
+    
+print(sum_r(5))
+
+#3) calcular el producto de dos numeros enteros dados
+# prod(n,m) = n + prod(n, m-1)          -> n, m = 0 -> 0
+
+def prod(n : int, m : int) -> int:
+    if m==0:
+        return 0
+    else:
+        return n + prod(n, m-1)
+
+print(prod(2,3))
+
+#7) h(n) ? 1/n + 1/(n-1) + 1/(n-2) + ... + 1  n = 1 -> 1
+
+def serie_h(num : float) -> float:
+    if num == 1:
+        return 1
+    else:
+        return 1/num + serie_h(num-1)
+    
+print(serie_h(4))
+
+#4) pot(n,m) = n ** m               -> pot(0,m) = 0, pot(n, 0) = 1
+
+def pot(n : int, m : int) -> int:
+    if n == 0:
+        return 0
+    elif m == 0:
+        return 1
+    else:
+        return n * pot(n, m-1)
+
+print(pot(5,3))
+
+#10) contar la cantidad de dígitos de un número entero
+# 523 -> 52     5           if n<10 = 1
+        # +1      +1
+
+def cont(n: int) -> int:
+    if n <=10:
+        return 1
+    else:
+        return 1 + cont(n // 10)
+
+print(cont(5))
+print(cont(52))
+print(cont(523))
+print(cont(5234))
+
+#11) invertir número entero sin convertirlo a cadena
+
+
+
+#14) suma de los dígitos de un número
+#321 = 3 + 2 + 1                        n < 10 return n
+
+
+#6) invertir secuencia de caracteres
+#invertir(hola) = a + invertir(hol) -> cadena = ''
+
+def invertir(cadena: str) -> str:
+    if cadena == '':
+        return cadena
+    else:
+        return cadena[-1] + invertir(cadena[:-1]) # se aplicó slicing, que toma porciones de la cadena
+
+print(invertir('casa'))
